b9e6f7a09ffc48266f50345675aac295a767fdfb
Sama807 <samarafaqat091@gmail.com>
2024-01-05 01:06:00 +0500

    alternatePositiveAndNegative working

---
+++ alternatePosNeg.cpp
@@ -0,0 +1,61 @@
+
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+void rearrange(int arr[], int n) {
+    
+    int result[n];
+    int j=0, k=0;
+    int index=0;
+    for(int i=0; i<n; i++){
+
+        if(arr[i] >=0 ){
+
+            result[k] = arr[i];
+            k++;
+        }
+    }
+    for(int i=0; i<n; i++){
+        if(arr[i] <0){
+            result[k] = arr[i];
+            k++;
+        }
+    }
+
+    for(int i=0, l=0; l<n; i=i+2, l++){
+        
+        if(index!=n){
+            arr[i] = result[l];
+        }
+        
+        while(result[index] >=0 && index!=n){
+            index++;
+        }
+        if(index!=n){
+            j=i+1;
+            arr[j] = result[index];
+            index++;
+        }
+        if(index==n+1){
+            int m= i;
+            while(l!=n){
+                arr[m] = result[l];
+                m++;
+                l++;
+            }
+        }
+
+    }
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+       
+      
+}
+
+int main(){
+
+    int arr[] = {-5,-2,5,2,4,7,1,8,0,-8};
+    rearrange(arr, 10);
+}
